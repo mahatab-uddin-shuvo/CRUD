f2ac4ca71fbb38f9d0d8c95c834d5dc032dae89e
mahatab-uddin-shuvo <shuvomahi12@gmail.com>
2021-06-28 01:49:03 +0600

    final

--- Client_1/src/Components/Home.jsx
+++
@@ -1,8 +0,0 @@
-import HomePage from '../Assets/images/homepage.jpeg'
-
-const Home = () => {
-  return(
-    <img src ={HomePage} style={{width:'30%',margin:'80px 0 0 35%'}}/>
-    )
-}
-export default Home;
